13b067c7cc60623cb70742be33b3674edaf1d061
Josh Moretti <jmoretti09@gmail.com>
2015-08-09 21:13:28 -0400

    final commit

--- fizzbuzz + beer/99 bottles of beer/scripts/scripts.js
+++ fizzbuzz + beer/99 bottles of beer/scripts/scripts.js
@@ -3,7 +3,7 @@ function beer (){
 		console.log(i + " Bottles of beer of the wall, " + i + " Bottles of beer of the wall... " 
 			+ "take one down and pass it around, ") 
 	if (i === 0) {
-		console.log(i + " bottles of beer on the wall! " + "Your outta beer ya drunk!")
+		console.log(i + " bottles of beer on the wall! " + "You're outta beer ya drunk!")
 	}
 }
 beer();
